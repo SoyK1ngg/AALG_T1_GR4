4ee0d6cbfcda6e264c48400baa7c1da9b289ab51
Johao Velasquez <aurerio-casilla64@hotmail.com>
2026-04-25 01:35:46 -0500

    equipo.py

--- equipo.py
+++ equipo.py
@@ -11,21 +11,25 @@ equipo1 = Equipo("Los Tigres")
 equipo2 = Equipo("Las Aguilas")
 
 def RegistraSet(ganador):
+    termino = False
     if ganador == 1:
-        ganadorEquipo = equipo1
-        perdedorEquipo = equipo2
-    else:
-        ganadorEquipo = equipo2
-        perdedorEquipo = equipo1
-    ganadorEquipo.setsGanados = ganadorEquipo.setsGanados + 1
-    if ganadorEquipo.setsGanados == 3:
-        ganadorEquipo.partidosGanados = ganadorEquipo.partidosGanados + 1
-        perdedorEquipo.partidosPerdidos = perdedorEquipo.partidosPerdidos + 1
-        ganadorEquipo.setsGanados = 0   
-        perdedorEquipo.setsGanados = 0  
-        print("  >> " + ganadorEquipo.nombre + " gano el partido!")
-        return True
-    return False
+        equipo1.setsGanados = equipo1.setsGanados + 1
+        if equipo1.setsGanados == 3:
+            equipo1.partidosGanados = equipo1.partidosGanados + 1
+            equipo2.partidosPerdidos = equipo2.partidosPerdidos + 1
+            print("  >> " + equipo1.nombre + " gano el partido!")
+            termino = True
+    elif ganador == 2:
+        equipo2.setsGanados = equipo2.setsGanados + 1
+        if equipo2.setsGanados == 3:
+            equipo2.partidosGanados = equipo2.partidosGanados + 1
+            equipo1.partidosPerdidos = equipo1.partidosPerdidos + 1
+            print("  >> " + equipo2.nombre + " gano el partido!")
+            termino = True
+    if termino == True:
+        equipo1.setsGanados = 0
+        equipo2.setsGanados = 0
+    return termino
 
 def Puntos():
     return random.randint(10, 28)
@@ -38,20 +42,21 @@ def JugarPartido():
     equipo2.setsGanados = 0
     numSet = 1
     terminoPartido = False
-    while not terminoPartido:  
+    while terminoPartido == False:
         p1 = Puntos()
         p2 = Puntos()
+
         while (p1 < 25 and p2 < 25) or p1 == p2:
             p1 = p1 + PuntosExtras()
             p2 = p2 + PuntosExtras()
+
         if p1 > p2:
-            ganador = 1
-            nombreGanador = equipo1.nombre
+            print("  Set " + str(numSet) + ": " + equipo1.nombre + " " + str(p1) + " - " + str(p2) + " " + equipo2.nombre + " -> Gana " + equipo1.nombre)
+            terminoPartido = RegistraSet(1)
         else:
-            ganador = 2
-            nombreGanador = equipo2.nombre
-        print("  Set " + str(numSet) + ": " + equipo1.nombre + " " + str(p1) + " - " + str(p2) + " " + equipo2.nombre + " -> Gana " + nombreGanador)
-        terminoPartido = RegistraSet(ganador)
+            print("  Set " + str(numSet) + ": " + equipo1.nombre + " " + str(p1) + " - " + str(p2) + " " + equipo2.nombre + " -> Gana " + equipo2.nombre)
+            terminoPartido = RegistraSet(2)
+
         numSet = numSet + 1
 
 def ResultadoTorneo():
@@ -59,6 +64,19 @@ def ResultadoTorneo():
     print(equipo1.nombre + ": Ganados = " + str(equipo1.partidosGanados) + " | Perdidos = " + str(equipo1.partidosPerdidos))
     print(equipo2.nombre + ": Ganados = " + str(equipo2.partidosGanados) + " | Perdidos = " + str(equipo2.partidosPerdidos))
     print("================================")
+
+    if equipo1.partidosGanados > equipo2.partidosGanados:
+        print(f"{equipo1.nombre} son los campeones")
+    elif equipo2.partidosGanados > equipo1.partidosGanados:
+        print(f"{equipo2.nombre} son los campeones")
+    else:
+        print("PARTIDO DE DESEMPATE")
+        print("================================")
+        JugarPartido()
+        if equipo1.partidosGanados > equipo2.partidosGanados:
+            print(f"{equipo1.nombre} son los campeones")
+        elif equipo2.partidosGanados > equipo1.partidosGanados:
+            print(f"{equipo2.nombre} son los campeones") 
 
 n = int(input("¿Cuantos partidos se jugaran? "))
 i = 1
